7774da1e54d73b2c4748bfdc1d2357397a215ccb
montimaj <monti.majumdar@gmail.com>
2015-08-18 10:39:54 +0530

    Added QRCode module in app. Removed QRCode decoding feature in PC version

--- src/stegano/utilities/QRCode.java
+++ src/stegano/utilities/QRCode.java
@@ -2,27 +2,18 @@ package stegano.utilities;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.imageio.ImageIO;
-
 import stegano.Encrypt;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.DecodeHintType;
 import com.google.zxing.EncodeHintType;
-import com.google.zxing.Result;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.QRCodeReader;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
@@ -35,10 +26,6 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 public class QRCode 
 {
 	public String fname;
-	public QRCode(String file, String dir, boolean flag) throws Exception
-	{
-		readQRCode(file, dir, flag);
-	}	
 	public QRCode(String input, String dir, int n) throws Exception
 	{
 		gen_qrcode(input, dir, n);
@@ -77,27 +64,5 @@ public class QRCode
 	{	   
 		BitMatrix matrix = new QRCodeWriter().encode(data, BarcodeFormat.QR_CODE, qrw, qrh, hint_map); 
 	    MatrixToImageWriter.writeToFile(matrix, "png",new File(file));
-	}
-	private void readQRCode(String file, String dir, boolean flag) throws Exception 
-	{		
-		Map<DecodeHintType, ErrorCorrectionLevel> hint_map=new HashMap<DecodeHintType, ErrorCorrectionLevel>();
-		hint_map.put(DecodeHintType.TRY_HARDER, ErrorCorrectionLevel.L);
-		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(file)))));
-		Result qr_result = new QRCodeReader().decode(bitmap, hint_map);
-		String data=qr_result.getText();
-		FileOutputStream fos;
-		if(!flag)
-		{
-			fname=dir+"/decoded.png";
-			fos=new FileOutputStream(fname);
-			fos.write(data.getBytes("ISO-8859-1"));
-		}
-		else
-		{
-			fname=dir+"/decoded_key.txt";
-			fos=new FileOutputStream(fname);
-			fos.write(data.getBytes());
-		}	
-		fos.close();				
-	}
+	}	
 }
